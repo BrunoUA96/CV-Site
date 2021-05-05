79f224c03432e90dd19a08d22eb0aaf83fb08dc6
Ostap <ostapklysa@gmail.com>
2021-05-05 15:25:24 +0100

    js tabs function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log('Olá');
+addEventListener('DOMContentLoaded', () => {
+   let tabsNav = document.querySelectorAll('.tabs_nav_item'),
+      tabsContent = document.querySelectorAll('.tab_content');
+   tabsContent[0].classList.add('show');
+
+   tabsNav.forEach((tab, index) => {
+      tab.addEventListener('click', () => {
+         tabsContent.forEach((content, contentIndex) => {
+            content.classList.remove('show');
+            tabsNav[contentIndex].classList.remove('tabs_nav_item--active');
+         });
+
+         tab.classList.add('tabs_nav_item--active');
+         tabsContent[index].classList.add('show');
+      });
+   });
+});
